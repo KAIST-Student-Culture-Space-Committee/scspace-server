bd3a8a1fc685941d674347b7262d1e78741e2b0d
h1ghl1kh7 <hyilim.kim@gmail.com>
2026-02-16 00:45:15 +0900

    DELETE: swagger

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,5 +1,4 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
-import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import {
   AdminGuard,
   DelegatorGuard,
@@ -8,103 +7,27 @@ import {
   MemberGuardWithReservation,
   UserGuard,
 } from '@scspace-server/feature/auth/jwt/jwt.guard';
-import { AuthGuard } from '@nestjs/passport';
 
 export function AuthAdmin() {
-  return applyDecorators(
-    UseGuards(AdminGuard),
-    ApiBearerAuth(),
-    ApiResponse({
-      status: 200,
-      description: '성공',
-    }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
+  return applyDecorators(UseGuards(AdminGuard));
 }
 
 export function AuthManager() {
-  return applyDecorators(
-    UseGuards(ManagerGuard),
-    ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '성공' }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
+  return applyDecorators(UseGuards(ManagerGuard));
 }
 
 export function AuthUser() {
-  return applyDecorators(
-    UseGuards(UserGuard),
-    ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '성공' }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
+  return applyDecorators(UseGuards(UserGuard));
 }
 
 export function AuthMember() {
-  return applyDecorators(
-    UseGuards(MemberGuard),
-    ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '성공' }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
+  return applyDecorators(UseGuards(MemberGuard));
 }
 
 export function AuthMemberWithReservation() {
-  return applyDecorators(
-    UseGuards(MemberGuardWithReservation),
-    ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '성공' }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
+  return applyDecorators(UseGuards(MemberGuardWithReservation));
 }
 
 export function AuthDelegator() {
-  return applyDecorators(
-    UseGuards(DelegatorGuard),
-    ApiBearerAuth(),
-    ApiResponse({ status: 200, description: '성공' }),
-    ApiResponse({
-      status: 401,
-      description: '로그인이 필요합니다.',
-    }),
-    ApiResponse({
-      status: 403,
-      description: '권한이 없습니다.',
-    }),
-  );
-} 
+  return applyDecorators(UseGuards(DelegatorGuard));
+}

d38bdc875e26cd9ba92e0d8e5180aed2f553a12b
h1ghl1kh7 <hyilim.kim@gmail.com>
2026-02-16 00:56:21 +0900

    UPDATE: redis

--- src/db/redis/redis.provider.ts
+++ src/db/redis/redis.provider.ts
@@ -9,9 +9,11 @@ export const redisProvider: Provider[] = [
   {
     provide: REDIS_CLIENT,
     useFactory: (configService: ConfigService) => {
+      const password = configService.get<string>('REDIS_PWD');
       return new Redis({
         host: configService.get<string>('REDIS_HOST', 'localhost'),
         port: configService.get<number>('REDIS_PORT', 6379),
+        ...(password ? { password } : {}),
       });
     },
     inject: [ConfigService],
